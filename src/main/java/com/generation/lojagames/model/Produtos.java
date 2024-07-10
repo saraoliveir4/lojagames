cdd614c2c2a38d9f601b7d8147a13354c7719122
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-07-10 18:05:34 -0300

    first commit

---
+++ src/main/java/com/generation/lojagames/model/Produtos.java
@@ -0,0 +1,77 @@
+package com.generation.lojagames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo Nome deve conter no mínimo 5 e no máximo 100 caracteres")
+	private String nome;
+
+	@NotNull(message = "O atributo Valor Unitário é obrigatório!")
+	private Float valorUnitario;
+
+	@NotNull(message = "O atributo Quantidade é obrigatório!")
+	private Integer quantidade;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(Float valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
